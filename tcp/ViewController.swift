592a64eb645a82b5d08135483ef93a6e602156f3
Franky Liang <fliangz96@gmail.com>
2017-07-14 16:10:24 -0400

    Begin working on communication between multiple devices

--- tcp/ViewController.swift
+++ tcp/ViewController.swift
@@ -12,12 +12,13 @@ import SwiftyJSON
 
 class ViewController: UIViewController, GCDAsyncSocketDelegate {
     
-    @IBOutlet weak var sendButton: UIButton!
+    let cluster = ["192.168.10.57", "192.168.10.58", "192.168.10.60"]
     var tcpSendSocket: GCDAsyncSocket?
     var tcpReceiveSocket: GCDAsyncSocket?
     
-    
+    @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var messageTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,15 +36,45 @@ class ViewController: UIViewController, GCDAsyncSocketDelegate {
         receiveMessages()
         
     }
+
+    @IBAction func touchSendButton(_ sender: Any) {
+        for server in cluster {
+            if (server == getIFAddresses()[1]) {
+                continue
+            }
+            sendMessage(server: server)
+        }
+    }
     
     func socket(_ sock: GCDAsyncSocket, didConnectToHost host: String, port: UInt16) {
         print("Connected to: " + host + " " + port.description)
     }
     
-    func sendMessage() {
+    func socket(_ sock: GCDAsyncSocket, didWriteDataWithTag tag: Int) {
+        print("WROTEE")
+    }
+    
+    func socket(_ sock: GCDAsyncSocket, didAcceptNewSocket newSocket: GCDAsyncSocket) {
+        print("WHAT")
+        tcpReceiveSocket = newSocket
+        receiveMessages()
+    }
+    
+    func socket(_ sock: GCDAsyncSocket, didRead data: Data, withTag tag: Int) {
+        let receiveJson = JSON(data: data)
+        print(receiveJson)
+        print("READ!")
+        if (receiveJson["type"].stringValue == "message") {
+            DispatchQueue.main.async {
+                self.messageTextView.text = self.messageTextView.text + receiveJson["message"].stringValue
+            }
+            receiveMessages()
+        }
+    }
+
+    func sendMessage(server: String) {
         do {
-//            try tcpSendSocket?.connect(toHost: "192.168.10.60", onPort: 20011) // frank tank
-            try tcpSendSocket?.connect(toHost: "192.168.10.58", onPort: 20011) // ipad
+            try tcpSendSocket?.connect(toHost: server, onPort: 20011)
         } catch {
             print(error)
         }
@@ -59,46 +90,43 @@ class ViewController: UIViewController, GCDAsyncSocketDelegate {
         
         tcpSendSocket?.write(sendData, withTimeout: -1, tag: 0)
     }
-    
-    func socket(_ sock: GCDAsyncSocket, didWriteDataWithTag tag: Int) {
-        print("WROTEE")
-    }
-    
-    func socket(_ sock: GCDAsyncSocket, didAcceptNewSocket newSocket: GCDAsyncSocket) {
-        print("WHAT")
-        tcpReceiveSocket = newSocket
-        receiveMessages()
-    }
-    
+
     func receiveMessages() {
-//        DispatchQueue.main.async {
-//            while true {
-//                self.tcpSocket?.readData(withTimeout: -1, tag: 0)
-//            }
-            tcpReceiveSocket?.readData(withTimeout: -1, tag: 0)
-//        }
+        //        DispatchQueue.main.async {
+        tcpReceiveSocket?.readData(withTimeout: -1, tag: 0)
+        //        }
     }
     
-    func socket(_ sock: GCDAsyncSocket, didRead data: Data, withTag tag: Int) {
-        let receiveJson = JSON(data: data)
-        print(receiveJson)
-        print("READ!")
-        if (receiveJson["type"].stringValue == "message") {
-            DispatchQueue.main.async {
-                self.messageTextView.text = self.messageTextView.text + receiveJson["message"].stringValue
+    func getIFAddresses() -> [String] {
+        var addresses = [String]()
+        
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return [] }
+        guard let firstAddr = ifaddr else { return [] }
+        
+        // For each interface ...
+        for ptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let flags = Int32(ptr.pointee.ifa_flags)
+            let addr = ptr.pointee.ifa_addr.pointee
+            
+            // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
+            if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
+                if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
+                    
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    if (getnameinfo(ptr.pointee.ifa_addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
+                                    nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+                        let address = String(cString: hostname)
+                        addresses.append(address)
+                    }
+                }
             }
-            receiveMessages()
         }
+        freeifaddrs(ifaddr)
+        return addresses
     }
-
-    @IBAction func touchSendButton(_ sender: Any) {
-        sendMessage()
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
